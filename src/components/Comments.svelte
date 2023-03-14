a55c7a70cdf646e6f713197aed1b67aa3d360c48
Miguel Cano Santana <55782155+miguelcanosantana@users.noreply.github.com>
2023-03-14 02:11:10 +0100

    Replaced with giscus.

--- src/components/Comments.svelte
+++ src/components/Comments.svelte
@@ -1,18 +1,29 @@
 <!-- https://youtu.be/zEq8RHqiCe8 -->
 <!-- https://www.jamesperkins.dev/post/supercharge-your-astro-blog -->
+
+
 <script>
         import { onMount } from 'svelte'
         onMount(_ => {
             const s = document.createElement('script')
-            const tag = document.getElementById('utterances')
-            s.setAttribute('repo',"Bleeping-Dragon/Bleeping-Dragon-Astro-Web"),
-            s.setAttribute('issue-term',"pathname"),
-            s.setAttribute('label',"comments"),
-            s.setAttribute('theme',"github-dark"),
+            const tag = document.getElementById('giscus')
+            s.src = "https://giscus.app/client.js"
+            s.setAttribute('data-repo',"Bleeping-Dragon/Bleeping-Dragon-Astro-Web"),
+            s.setAttribute('data-repo-id',"DIC_kwDOIjEaS84CU2wQ"),
+            s.setAttribute('data-category',"General"),
+            s.setAttribute('data-category-id',"DIC_kwDOIjEaS84CU2wQ"),
+            s.setAttribute('data-mapping',"pathname"),
+            s.setAttribute('data-strict',"0"),
+            s.setAttribute('data-reactions-enabled',"1"),
+            s.setAttribute('data-emit-metadata',"0"),
+            s.setAttribute('data-input-position',"top"),
+            s.setAttribute('data-theme',"preferred_color_scheme"),
+            s.setAttribute('data-lang',"en"),
+            s.setAttribute('data-loading',"lazy"),
             s.setAttribute('crossorigin',"anonymous"),
-            s.src = 'https://utteranc.es/client.js',
+            s.setAttribute('async'),
             tag.parentNode.insertBefore(s, tag)
         })
 </script>
         
-<div id="utterances"></div>
+<div id="giscus"></div>
